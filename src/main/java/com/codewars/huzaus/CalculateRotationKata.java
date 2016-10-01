fae39b4bc58072409615f3fcd4c379a09dc1e94a
huzaus <4kc8iED123>
2016-10-01 13:27:24 +0200

    Implementation CalculateRotationKata

---
+++ src/main/java/com/codewars/huzaus/CalculateRotationKata.java
@@ -0,0 +1,17 @@
+package com.codewars.huzaus;
+
+public class CalculateRotationKata {
+    static int shiftedDiff(String first, String second) {
+        validate(first, second);
+        if (first.length() == second.length()) {
+            return second.concat(second).indexOf(first);
+        }
+        return -1;
+    }
+
+    private static void validate(String first, String second) {
+        if (first == null || second == null) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
---
+++ src/test/groovy/com/codewars/huzaus/CalculateRotationKataSpec.groovy
@@ -0,0 +1,35 @@
+package com.codewars.huzaus
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+@Unroll
+class CalculateRotationKataSpec extends Specification {
+
+    def "should throw IllegalArgumentException when: shiftedDiff(#first, #second)"() {
+        when:
+        CalculateRotationKata.shiftedDiff(first, second)
+        then:
+        thrown(IllegalArgumentException)
+        where:
+        first    | second
+        null     | 'string'
+        'string' | null
+    }
+
+    def "shiftedDiff('#first', '#second') == #result"() {
+        expect:
+        CalculateRotationKata.shiftedDiff(first, second) == result
+        where:
+        first     | second    || result
+        'a'       | 'ab'      || -1
+        'ab'      | 'ab'      || 0
+        'ab'      | 'ba'      || 1
+        'abc'     | 'bca'     || 2
+        'bca'     | 'abc'     || 1
+        'abcdefg' | 'gabcdef' || 1
+        'gabcdef' | 'abcdefg' || 6
+        'dog'     | 'god'     || -1
+        'dog'     | 'Dog'     || -1
+    }
+}
